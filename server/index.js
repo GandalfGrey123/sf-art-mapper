fffb18e5d3c7bf8e16f74ce8b2a1740b5423a60b
Marcus <gandalfgrey@gmx.com>
2019-06-19 15:52:57 -0700

    add temp view files

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+const router = express.Router();
+
+
+//temp test
+app.set(`view engine`, `ejs`);
+app.use(express.static(__dirname + '/views'));
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());    
+app.use('/',router.get('/', (req,res) =>{
+  res.render('index.html.ejs');
+}));
+
+
+app.listen(443, '127.0.0.1', () => {
+  console.log('express server started')
+});
