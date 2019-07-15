85385d204cb135604b3fd4171e92217a7308d5a6
Marcus <gandalfgrey@gmx.com>
2019-07-15 00:58:26 -0700

    added new route paths

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,21 @@ import React , { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import AppBar from './modules/app-bar/AppBar';
-import MainContainer from './modules/main-container/MainContainer'
+import MainContainer from './modules/main-containers/MainContainer'
+import HomePage from './modules/home-containers/home-content/HomePage'
+import DashBoardContainer from './modules/dashboard-containers/DashBoardContainer'
 
-class App extends Component {
 
+class App extends Component{
   render(){
    return(
    	<BrowserRouter>
    	<div>
    	  <AppBar />    
    	  <Switch>
-        <Route path={'/'} component={MainContainer} />
+        <Route path={'/home'} component={HomePage} />
+        <Route path={'/dash'} component={MainContainer} />
+        <Route path={'/'} component={DashBoardContainer} />
       </Switch>     
    	</div>
    	</BrowserRouter>
