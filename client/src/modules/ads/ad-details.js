16cec03975fc092321eb3e39ccc751cd034b3afd
Marcus <gandalfgrey@gmx.com>
2019-07-15 01:05:08 -0700

    moved ad components

---
+++ client/src/modules/ads/ad-details.js
@@ -0,0 +1,39 @@
+      // <Col xs={12} sm={7} md={7} lg={7} xl={7}>
+      //   <Card className="">
+
+      //     <Card.Title>
+      //       <h2 className="p-2 mt-3 text-center">Local Art Event</h2>
+      //     </Card.Title>
+          
+      //     <Card.Img 
+      //       variant="top" 
+      //       src="holder.js/100px180" 
+      //       className="pt-4 pl-4 pr-4 w-70"
+      //     />
+      //     <div className="p-4">
+      //        <Card.Subtitle className="text-muted float-right p-2 border drop-shadow">100 people going</Card.Subtitle>
+      //        <hr />
+      //        <Card.Title className="mt-5">Hours and Location</Card.Title>             
+      //         <Card.Text>
+      //           <div>
+      //             <p> May 13th , 10:00am - 5:00pm </p>
+      //             <p> 1896 11th ave </p>
+      //             <p> San Francisco, CA 94122 </p>
+      //           </div>
+      //         </Card.Text>
+      //         <Form>
+      //            <Form.Group controlId="formBasicChecbox">
+      //            <Form.Row>
+      //              <Form.Check className="mr-2" type="checkbox" label="RSVP Going" />
+      //              <Form.Check className="ml-2" type="checkbox" label="Save Event" />
+      //            </Form.Row>
+      //            </Form.Group>
+      //          </Form>
+      //      </div>
+      //      <div className="p-4">
+      //      <Button variant="outline-secondary w-50">
+      //       Event Details Summary
+      //      </Button>
+      //      </div>
+      //   </Card>
+      // </Col>
